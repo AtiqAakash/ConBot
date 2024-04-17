89a0775d38c5a4117a4502cd913565e6dbef6362
Md Atiqur Rahman Aakash <114288192+AtiqAakash@users.noreply.github.com>
2024-04-17 15:31:27 +0200

    Update ConBot.ps1
    Bug Fixed

--- ConBot.ps1
+++ ConBot.ps1
@@ -27,7 +27,7 @@ Write-Output "Downloading Cygwin x64 (4/12)"
 Start-BitsTransfer -Source "https://cygwin.com/setup-x86_64.exe" -Destination "$PSScriptRoot\setup-x86_64.exe"
 
 Write-Output "Downloading ARM GCC Compiler 12-2020-Q4-Major (5/12)"
-Start-BitsTransfer -Source "https://firmware.ardupilot.org/Tools/STM32-tools/gcc-arm-none-eabi-12-2020-q4-major-win32.exe" -Destination "$PSScriptRoot\gcc-arm-none-eabi-12-2020-q4-major-win32.exe"
+Start-BitsTransfer -Source "https://firmware.ardupilot.org/Tools/STM32-tools/gcc-arm-none-eabi-10-2020-q4-major-win32.exe" -Destination "$PSScriptRoot\gcc-arm-none-eabi-12-2020-q4-major-win32.exe"
 
 Write-Output "Installing Cygwin x64 (6/12)"
 Start-Process -wait -FilePath $PSScriptRoot\setup-x86_64.exe -ArgumentList "--root=C:\cygwin64 --no-startmenu --local-package-dir=$env:USERPROFILE\Downloads --site=http://cygwin.mirror.constant.com --packages autoconf,automake,ccache,cygwin32-gcc-g++,gcc-g++=7.4.0-1,libgcc1=7.4.0.1,gcc-core=7.4.0-1,git,libtool,make,gawk,libexpat-devel,libxml2-devel,python39,python39-future,python39-lxml,python39-pip,python39-pip3,libxslt-devel,python39-devel,procps-ng,zip,gdb,ddd,xterm --quiet-mode"
